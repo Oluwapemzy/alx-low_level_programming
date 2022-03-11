c0ce02979fcfa97e1dfd8719183b25ba14f82163
Oluwapemzy <ojeksman2000@gmail.com>
2022-03-11 13:06:35 -0800

    whether positive or negative

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
-* main - main block
+* main - Entry point
 *
-* Return : 0
-*
+* Return : Always 0 (Success)
 */
 int main(void)
 {
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+**Tis file is mandatory in all projects**
