d76bb526694b5435b0e6ebd7b27135aa9fc91de6
Oluwapemzy <ojeksman2000@gmail.com>
2022-03-11 18:59:14 -0800

    reverse letter

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -13,5 +13,5 @@ putchar(i);
 i++;
 }
 putchar('\n');
-return(0);
+return (0);
 }
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+int c = 'z';
+while (c >= 'a')
+{
+putchar(c);
+c--;
+}
+putchar('\n');
+return (0);
+} 
