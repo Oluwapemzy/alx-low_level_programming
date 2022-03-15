bec8b6cf6e39747ffca727b779be25034c2303f9
Oluwapemzy <ojeksman2000@gmail.com>
2022-03-15 09:21:28 -0700

    print alphabet 10 times fixed

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,19 @@
 #include "main.h"
 /**
- * main - check the code.
+ * print_alphabet_x10 - check the code.
  *
  * Return: Always 0.
  */
 void print_alphabet_x10(void)
 {
 char alphabets[] = "abcdefghijklmnopqrstuvwxyz\n";
-int i = 0;
 int c = 0;
 while (c < 10)
 {
+int i = 0;
 while (alphabets[i] != 0)
 {
-
-  _putchar(alphabets[i]);
+_putchar(alphabets[i]);
 i++;
 }
 c++;
