c6175c0e87a0e93764117ffd55f7a8336b7ac184
Oluwapemzy <ojeksman2000@gmail.com>
2022-03-16 05:05:31 -0700

    fixed times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -35,32 +35,3 @@ void times_table(void)
 	}
 
 }
-/*void times_table(void)
-{
-int firstloop, secondloop, product;
-for (firstloop = 0; firstloop <= 9; firstloop++)
-{
-_putchar('0');
-_putchar(',');
-_putchar(' ');
-for (secondloop = 0; secondloop <= 9; secondloop++)
-{
-product = firstloop * secondloop;
-if ((product / 10) > 0)
-{
-_putchar ((product / 10));
-}
-else
-{
-_putchar(' ');
-}
-_putchar ((product % 10) + '0');
-if (secondloop != 9)
-{
-_putchar (',');
-_putchar (' ');
-}
-}
-_putchar ('\n');
-}
-}*/
