ce9776c26fd64e4783dbc756497005502719bfdc
Oluwapemzy <ojeksman2000@gmail.com>
2022-03-16 05:01:53 -0700

    time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,15 +5,50 @@
  */
 void times_table(void)
 {
+	int rone, cone, d;
+
+	for (rone = 0; rone <= 9; rone++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (cone = 1; cone <= 9; cone++)
+		{
+			d = (rone * cone);
+			if ((d / 10) > 0)
+			{
+				_putchar((d / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((d % 10) + '0');
+
+			if (cone < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+
+}
+/*void times_table(void)
+{
 int firstloop, secondloop, product;
 for (firstloop = 0; firstloop <= 9; firstloop++)
 {
+_putchar('0');
+_putchar(',');
+_putchar(' ');
 for (secondloop = 0; secondloop <= 9; secondloop++)
 {
 product = firstloop * secondloop;
 if ((product / 10) > 0)
 {
-_putchar ((product / 10) + '0');
+_putchar ((product / 10));
 }
 else
 {
@@ -28,4 +63,4 @@ _putchar (' ');
 }
 _putchar ('\n');
 }
-}
+}*/
