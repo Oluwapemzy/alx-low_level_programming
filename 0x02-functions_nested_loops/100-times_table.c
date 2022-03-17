6a0286e0fddbe0177ccc55c8834c115bab7250fc
Oluwapemzy <ojeksman2000@gmail.com>
2022-03-16 21:42:04 -0700

    not all fixed up

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ *times_table-print time table
+ *Return: void
+ */
+void print_times_table(int n)
+{
+if (n > 15 || n < 0)
+{
+}
+else
+{
+int loop1, loop2, d;
+for (loop1 = 0; loop1 <=n; loop1++)
+/*_putchar('0');
+_putchar(',');
+_putchar(' ');*/
+for (loop2 = 0; loop2 <= n; loop2++)
+{
+d = loop1 * loop2;
+if ((d / 10) > 0)
+{
+_putchar((d / 10) + '0');
+}
+else
+{
+_putchar(' ');
+}
+_putchar((d % 10) + '0');
+if (loop2 < n)
+{
+_putchar(',');
+_putchar(' ');
+}
+if (loop2 == n)
+{
+_putchar('\n');
+}
+}
+_putchar('\n');
+}
+/*_putchar('\n');*/
+}
