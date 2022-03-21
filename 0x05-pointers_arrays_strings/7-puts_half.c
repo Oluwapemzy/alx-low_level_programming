06d7c089f1a8b138e6ed993eaf8bb4340213bf52
Oluwapemzy <ojeksman2000@gmail.com>
2022-03-21 12:33:12 -0700

    betty style fixed only on task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,7 +9,7 @@ void puts_half(char *str)
 {
 int i, n, length;
 length = strlen(str);
- if (length % 2 == 0)
+if (length % 2 == 0)
 {
 n = (length - 1) / 2;
 }
@@ -17,7 +17,7 @@ else
 {
 n = length / 2;
 }
-for(i = n + 1; i <= length; i++)
+for (i = n + 1; i <= length; i++)
 {
 _putchar(str[i]);
 }
