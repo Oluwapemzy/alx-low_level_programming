c35e6f9bd12ac9706a25a15ba6d0461dd178c04d
Oluwapemzy <ojeksman2000@gmail.com>
2022-03-21 12:40:16 -0700

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,13 +9,13 @@ void puts_half(char *str)
 {
 int i, n, length;
 length = strlen(str);
-if (length % 2 == 0)
+if (length  % 2 == 0)
 {
 n = (length - 1) / 2;
 }
 else
 {
-n = length / 2;
+n = (length - 1) / 2;
 }
 for (i = n + 1; i <= length; i++)
 {
