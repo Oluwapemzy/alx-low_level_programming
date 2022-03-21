4449e36a46b7abb595c3e9ed9486dd1c87c3a155
Oluwapemzy <ojeksman2000@gmail.com>
2022-03-21 04:42:54 -0700

    successfully reversed string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ *print_rev-reverse a string
+ *@s: type char
+ *Return: void
+ */
+void print_rev(char *s)
+{
+int length = strlen(s);
+int i;
+for (i = length; i >= 0; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
