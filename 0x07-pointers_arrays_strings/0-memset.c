71d6d208ca663dc7395266928f487de17465996a
Oluwapemzy <ojeksman2000@gmail.com>
2022-03-28 20:46:15 -0700

    first task in project 0x07

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ *_memset - type char 
+ *@s: param to return
+ *@b: what to fill
+ *@n:number of bytes
+ *Return : return type char
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+s = memset(s, b, n);
+return (s);
+}
