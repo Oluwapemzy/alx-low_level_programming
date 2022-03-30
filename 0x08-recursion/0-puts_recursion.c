62b5245f6e884b462e4996fa8c6a551695d080f7
Oluwapemzy <ojeksman2000@gmail.com>
2022-03-30 02:37:33 -0700

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_puts_recursion- ouputs a string using puts
+ *@s: the string to output
+ *Return:type void
+ */
+void _puts_recursion(char *s)
+{
+puts(s);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,2 @@
+**This file is mandatory in ALL projects**
+This project is all about Recursion that is a function calling itself. take for example, factorial.
