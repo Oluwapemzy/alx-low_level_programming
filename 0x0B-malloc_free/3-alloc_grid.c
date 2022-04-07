69c345b8b4d92a1ce7c0889dd168e7eb9b5441e8
Oluwapemzy <ojeksman2000@gmail.com>
2022-04-06 19:55:30 -0700

    3-alloc_grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * alloc_grid-create a two dimensional array
+ * @width: width of matrix no of cols
+ * @height: height of mat no of rows
+ * Return: pointer to matrixcreated
+ */
+int **alloc_grid(int width, int height)
+{
+int **ar;
+int i, j;
+if (width <= 0 || height <= 0)
+{
+return (NULL);
+}
+ar = malloc(sizeof(int *) * height);
+if (ar == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < height; i++)
+{
+arr[i] = (int *) malloc(sizeof(int) * width);
+if (ar[i] == NULL)
+{
+free(ar);
+for (j = 0; j <= i; j++)
+{
+free((ar[j]));
+return (NULL);
+}
+}
+}
+for (i = 0; i < height; i++)
+{
+for (j = 0; j < width; j++)
+{
+ar[i][j] = 0;
+}
+}
+return (ar);
+}
