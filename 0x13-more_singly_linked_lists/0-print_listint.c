c6bb0f108cca18fea765aab42fed6b2a76b1b19c
Oluwapemzy <ojeksman2000@gmail.com>
2022-04-25 02:52:43 -0700

    0. Print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ *print_listint - funtion that prints all elements of list
+ *@h: the singly list
+ *Return: type size_t, number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+size_t nodes = 0;
+while (h != NULL)
+{
+if (h->n)
+{
+printf("%d\n", h->n);
+}
+h = h->next;
+nodes++;
+}
+return (nodes);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+# Alx project - More Singly linked lists
+## Task 0- to *Print list*
+Function that prints all the elements of a listint_t list.
