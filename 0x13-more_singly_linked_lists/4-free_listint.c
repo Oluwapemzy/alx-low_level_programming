b4b54200d22538350bc0e3d3dbe685c84bad7e69
Oluwapemzy <ojeksman2000@gmail.com>
2022-04-27 02:20:35 -0700

    4-free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ *free_listint- frees a list
+ *@head: head of list
+ *Return: void return type
+ */
+void free_listint(listint_t *head)
+{
+listint_t *temp;
+while (head)
+{
+temp = head;
+head = head->next;
+free (temp);
+}
+}
