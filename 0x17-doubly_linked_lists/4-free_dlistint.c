f5257e49113b540b8f47bd27955851bd2e07ac71
Oluwapemzy <ojeksman2000@gmail.com>
2022-06-11 09:45:03 -0700

    all tasks added

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_dlistint- frees a list
+ * @head: head address
+ * Return: nothing
+ */
+void free_dlistint(dlistint_t *head)
+{
+    dlistint_t *start, *check;
+    start = head;
+    while (start != NULL)
+    {
+        check = start->next;
+        free(start);
+        start = check;
+    }
+}
---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * sum_dlistint- sums all data in a list
+ * @head: head of list
+ * Return: sum of all data
+ */
+int sum_dlistint(dlistint_t *head)
+{
+    int sum = 0;
+    while (head != NULL)
+    {
+        sum += head->n;
+        head = head->next;
+    }
+    return (sum);
+}
