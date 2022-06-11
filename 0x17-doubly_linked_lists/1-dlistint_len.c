d1ec9817e5f417412cc9b7c04794950ff469a371
Oluwapemzy <ojeksman2000@gmail.com>
2022-06-11 09:54:15 -0700

    fixed betty on task1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,19 +1,18 @@
 #include "lists.h"
-/*
-*print_dlistint- print double linked list
+/**
+*dlistint_len- print double linked list
 *@h: the head
 *Return: number of nodes
 */
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
-    int i;
-    size_t len = 0;
-    for (i = 0; h; i++)
-    {
-        /* code */
-        printf("%d\n",h->n);
-        h=h->next;
-        len++;
-    }
-    return (len);
+int i;
+size_t len = 0;
+for (i = 0; h; i++)
+{
+printf("%d\n", h->n);
+h = h->next;
+len++;
+}
+return (len);
 }
